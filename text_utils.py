e4dc05e232c6b206a6a94642918d7b0aa861945a
Jacob Silterra <jsilter@gmail.com>
2017-03-13 23:10:39 -0400

    Add text_utils, necessary for processing text

---
+++ text_utils.py
@@ -0,0 +1,68 @@
+#!/bin/bash
+from __future__ import division  # Python 2 users only
+
+import csv
+import random
+import datetime
+import os
+import re
+
+import numpy as np
+import nltk
+
+import keras
+from keras.utils import np_utils
+from keras.preprocessing import sequence
+
+
+def create_training_batch(generator, num_classes, max_input_length, max_batch_size=64, return_raw_text=False):
+    text_data = []
+    X_ = []
+    y_ = []
+    for info_dict in generator:
+        seq_data = info_dict['int_word_list']
+        _class = int(info_dict['class']) - 1
+        
+        text_data.append(info_dict['word_list'])
+        X_.append(seq_data)
+        y_.append(_class)
+        
+        if len(y_) >= max_batch_size:
+            break
+            
+    X_train = sequence.pad_sequences(X_, maxlen=max_input_length)
+    y_train = np_utils.to_categorical(y_, nb_classes=num_classes)
+        
+    if return_raw_text:
+        return X_train, y_train, text_data
+    else:
+        return X_train, y_train
+    
+    
+def create_desc_generator(input_path, word2id, indefinite=False, min_word_count=10):
+    columns = ['class', 'title','description']
+    text_field = 'description'
+    not_none = lambda x: x is not None
+    _finished = False
+    while not _finished:
+        csv_reader = csv.DictReader(open(input_path, 'r'), fieldnames=columns)
+        for ind, cur_dict in enumerate(csv_reader):
+            text = cur_dict[text_field].strip().lower()
+            # Don't love this but what can you do
+            text = text.decode("ascii","ignore").encode("ascii")
+            word_list = nltk.word_tokenize(text)
+            cur_dict['word_list'] = word_list
+            int_word_list = [word2id[w] for w in word_list if w in word2id]
+            if len(int_word_list) < min_word_count:
+                continue
+            cur_dict['int_word_list'] = int_word_list
+            yield cur_dict
+        
+        _finished = not indefinite
+        
+        
+def create_batch_generator(input_path, word2id, num_classes, max_input_length, batch_size, return_raw_text=False):
+    desc_generator = create_desc_generator(input_path, word2id, indefinite=True)
+    while True:
+        cur_batch = create_training_batch(desc_generator, num_classes, max_input_length, batch_size, return_raw_text=return_raw_text)
+        yield cur_batch
